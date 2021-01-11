47e1e1fd8eacd90c55ccc6ace382a3a24d40f8e3
Martin Najjar <martin_najjar@yahoo.com>
2021-01-11 23:54:24 +0300

    Add bootstrap cdn and card layout

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-
+require('./css/styles.css');
 const todo = require('./scripts/todoClass');
 const project = require('./scripts/project');
-
-// function initialize() {
-//   if (project) {
-//   }
-// }
 
 const to = new todo('Todo', 'description', '11,01,2021', 't', 'notes');
 const p = new project('Test');
 
-p.addTodo(to);
+const cardTitle = document.querySelector('.cardTitle');
+cardTitle.textContent = p.name;
 
-const doc = document.querySelector('#content');
-doc.textContent = p.todos[0].title;
+const ul = document.querySelector('.todos');
+const todoLi = document.createElement('li');
+todoLi.textContent = to.title;
+ul.appendChild(todoLi);
