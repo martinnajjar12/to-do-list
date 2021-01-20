bbdb7b73237dcf97fe81d59d8a2eaaec20459e05
Martin Najjar <martin_najjar@yahoo.com>
2021-01-20 19:38:06 +0300

    Test newProjectForm.js file

---
+++ src/test/newProjectForm.test.js
@@ -0,0 +1,7 @@
+import newProjectForm from '../scripts/newProjectForm';
+
+describe('newProjectForm', () => {
+  it('returns a HTML Object', () => {
+    expect(typeof newProjectForm).toBe('object');
+  });
+});
